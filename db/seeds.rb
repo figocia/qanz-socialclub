e0d965edf7d36afa52ebf590ae669a1002cc9280
figochen <figocia@hotmail.com>
2015-08-28 10:03:00 +1000

    seeding dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,68 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Competition.delete_all
+User.delete_all
+Game.delete_all
+Team.delete_all
+TeamMember.delete_all
+
+competition2015 = Competition.create(name: '2015 Foosball Competition')
+competition2014 = Competition.create(name: '2014 Foosball Competition', is_finished: true)
+
+# teams
+team1 = Team.create(name: 'Team1', competition: competition2015 ) 
+team2 = Team.create(name: 'Team2', competition: competition2015 ) 
+team3 = Team.create(name: 'Team3', competition: competition2015 ) 
+team4 = Team.create(name: 'Team4', competition: competition2015 ) 
+team5 = Team.create(name: 'Team5', competition: competition2015 ) 
+team6 = Team.create(name: 'Team6', competition: competition2015 ) 
+team7 = Team.create(name: 'Team7', competition: competition2015 ) 
+team8 = Team.create(name: 'Team8', competition: competition2015 ) 
+
+team10 = Team.create(name: 'Team8', competition: competition2014 ) 
+team11 = Team.create(name: 'Team7', competition: competition2014 ) 
+ 
+game1 = Game.create(round: 'Round4', competition: competition2015, team_one: team1, team_two: team5, is_finished: false)
+game2 = Game.create(round: 'Round5', competition: competition2015, team_one: team2, team_two: team6, is_finished: false)
+game3 = Game.create(round: 'Round6', competition: competition2015, team_one: team3, team_two: team7, is_finished: false)
+game4 = Game.create(round: 'Round7', competition: competition2015, team_one: team4, team_two: team8, is_finished: false)
+game5 = Game.create(round: 'Round1', competition: competition2015, team_one: team1, team_two: team2, is_finished: true, team_one_score: 10, team_two_score: 2)
+game6 = Game.create(round: 'Round2', competition: competition2015, team_one: team3, team_two: team4, is_finished: true, team_one_score: 10, team_two_score: 2)
+game7 = Game.create(round: 'Round3', competition: competition2015, team_one: team5, team_two: team6, is_finished: true, team_one_score: 10, team_two_score: 2)
+game8 = Game.create(round: 'Round4', competition: competition2015, team_one: team7, team_two: team8, is_finished: true, team_one_score: 10, team_two_score: 2)
+
+
+
+user1 = User.create(name: 'Figo', email: 'yufei.chen@3ds.com', is_member: false )
+user2 = User.create(name: 'Alice', email: 'yufei.chen@3ds.com', is_member: false )
+user3 = User.create(name: 'Bob', email: 'yufei.chen@3ds.com', is_member: false )
+user4 = User.create(name: 'Yufei', email: 'yufei.chen@3ds.com', is_member: false )
+user5 = User.create(name: 'Kenny', email: 'yufei.chen@3ds.com', is_member: false )
+user6 = User.create(name: 'CT', email: 'yufei.chen@3ds.com', is_member: false )
+user7 = User.create(name: 'Chee Jun', email: 'yufei.chen@3ds.com', is_member: false )
+user8 = User.create(name: 'Sher Jun', email: 'yufei.chen@3ds.com', is_member: false )
+user9 = User.create(name: 'Tim', email: 'yufei.chen@3ds.com', is_member: false )
+user10 = User.create(name: 'PG', email: 'yufei.chen@3ds.com', is_member: false )
+user11 = User.create(name: 'PS', email: 'yufei.chen@3ds.com', is_member: false )
+user12 = User.create(name: 'Selin', email: 'yufei.chen@3ds.com', is_member: false )
+user13 = User.create(name: 'Jemma', email: 'yufei.chen@3ds.com', is_member: false )
+user14 = User.create(name: 'Loris', email: 'yufei.chen@3ds.com', is_member: false )
+
+
+team_member1 = TeamMember.create(team: team1, member: user1)
+team_member2 = TeamMember.create(team: team1, member: user2)
+team_member3 = TeamMember.create(team: team1, member: user3)
+team_member4 = TeamMember.create(team: team1, member: user4)
+team_member5 = TeamMember.create(team: team2, member: user5)
+team_member6 = TeamMember.create(team: team2, member: user6)
+team_member7 = TeamMember.create(team: team2, member: user7)
+team_member8 = TeamMember.create(team: team2, member: user8)
+team_member9 = TeamMember.create(team: team3, member: user9)
+team_member10 = TeamMember.create(team: team3, member: user10)
+team_member11 = TeamMember.create(team: team3, member: user11)
+team_member12 = TeamMember.create(team: team3, member: user12)
+team_member13 = TeamMember.create(team: team4, member: user13)
+team_member14 = TeamMember.create(team: team4, member: user14)
