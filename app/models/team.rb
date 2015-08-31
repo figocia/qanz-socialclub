c20c4d1e75ae7cc42015e08c54ada691675fa6fa
figochen <figocia@hotmail.com>
2015-08-31 18:36:09 +1000

    team total score feature

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,5 +3,9 @@ class Team < ActiveRecord::Base
   has_many :team_members
   has_many :members, through: :team_members
   has_many :games, foreign_key: 'team_one_id'
-  has_many :inverse_games, foreign_key: 'team_two_id'  
+  has_many :inverse_games, class_name: 'Game', foreign_key: 'team_two_id'  
+
+  def total_score
+    games.map(&:team_one_score).inject(:+) + inverse_games.map(&:team_two_score).inject(:+)
+  end
 end
