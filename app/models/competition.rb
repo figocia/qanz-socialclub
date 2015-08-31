0b0a9047bd6edb9a851bbea7ce0887ce84db5582
figochen <figocia@hotmail.com>
2015-08-31 10:26:43 +1000

    leaderboard view

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,5 +1,7 @@
 class Competition < ActiveRecord::Base
-  has_many :games, -> {  order 'is_finished, created_at DESC' }
-  has_many :teams  
+  has_many :games, -> { order 'is_finished, created_at DESC' }
+  has_many :teams, -> { order 'total_score DESC, name' }
   
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: 'games#index'
-  get '/leadearboard', to: 'teams#index'
-  
+  get '/leaderboard', to: 'teams#index'
+
   resources :games, only: [:index, :show ]
   resources :teams, only: [:index, :show ]
 end
