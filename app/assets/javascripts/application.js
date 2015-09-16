d54a5060393560bf7fe933e14224f4dd2e59892a
figochen <figocia@hotmail.com>
2015-09-16 18:20:07 +1000

    error json in update score dialog

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,18 @@
 //= require bootstrap-sprockets
 //= require_tree .
 //= require leaderboard
+
+
+
+$(document).ajaxError(function(event,xhr,options,exc) {
+    
+    var errors = JSON.parse(xhr.responseText);
+    var er ="<ul>";
+    for(var i = 0; i < errors.length; i++){
+        var list = errors[i];
+        er += "<li>"+list+"</li>"
+    }
+    er+="</ul>"
+    $("#error_explanation").html(er);
+       
+});
