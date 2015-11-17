1a2925714a50533f179a482a09ab67174a0f2653
figochen <figocia@hotmail.com>
2015-11-17 18:34:50 +1100

    reset password controller done

--- app/controllers/reset_password_controller.rb
+++ app/controllers/reset_password_controller.rb
@@ -4,4 +4,17 @@ class ResetPasswordController < ApplicationController
     @user = User.find_by(reset_password_token: params[:reset_password_token])
     redirect_to token_expire_path if @user.nil?
   end
+
+  def create
+    user = User.find_by(reset_password_token: params[:reset_password_token])
+    user.password = params[:password]
+    if user.save
+      flash[:notice] = "Password reset done"
+      user.clear_reset_password_token
+      redirect_to login_path
+    else
+      flash[:error] = "Password must be minimun 5 character"
+      redirect_to new_reset_password_path(reset_password_token: params[:reset_password_token])
+    end
+  end
 end
--- spec/controllers/reset_password_controller_spec.rb
+++ spec/controllers/reset_password_controller_spec.rb
@@ -15,4 +15,35 @@ describe ResetPasswordController do
       expect(response).to redirect_to token_expire_path
     end
   end
+
+  describe 'POST Create' do
+    context 'with valid password' do
+      it 'resets the password' do
+        alice = Fabricate(:user, reset_password_token: '1234')
+        post :create, password: '1234567', reset_password_token: '1234' 
+        alice.reload
+        expect(alice.authenticate('1234567')).to eq(alice)
+      end  
+      it 'clears the password token' do
+        alice = Fabricate(:user, reset_password_token: '1234')
+        post :create, password: '1234567', reset_password_token: '1234' 
+        alice.reload
+        expect(alice.reset_password_token).to eq(nil)
+      end
+    end
+
+    context 'with invlaid password' do
+      it 'redirect to the show path' do
+        alice = Fabricate(:user, reset_password_token: '1234')
+        post :create, password: '123', reset_password_token: '1234' 
+        expect(response).to redirect_to new_reset_password_path(reset_password_token: alice.reset_password_token)
+      end
+
+      it 'errors out' do      
+        alice = Fabricate(:user, reset_password_token: '1234')
+        post :create, password: '123', reset_password_token: '1234' 
+        expect(flash[:error]).to eq("Password must be minimun 5 character")
+      end
+    end
+  end
 end
