4e876ada8b0505077bd0c4c3f13e4767ee7786ab
figochen <figocia@hotmail.com>
2016-01-17 19:11:24 +1100

    adding new competition create js

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,6 +1,8 @@
 class Competition < ActiveRecord::Base
   has_many :games, -> { order 'is_finished, created_at DESC' }
   has_many :teams
+
+  validates :name, presence: true, uniqueness: true
   
   def popular_teams
     teams.sort_by{|team| [ -team.total_score, team.name]}
