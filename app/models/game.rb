bd734c8f4e385d1ba1540d0f9895467d7d84c9e6
figochen <figocia@hotmail.com>
2016-01-22 00:32:06 +1100

    create single game action complete

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ActiveRecord::Base
   belongs_to :team_two, class_name: 'Team'
   belongs_to :competition
   belongs_to :round
-
+  
   validates_numericality_of :team_one_score, {only_integer: true, greater_than_or_equal_to: 0 }
   validates_numericality_of :team_two_score, {only_integer: true, greater_than_or_equal_to: 0 }  
   
