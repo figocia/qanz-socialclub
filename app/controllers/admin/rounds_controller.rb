65904d08add927b60f32e754475141d74b6f3e22
figochen <figocia@hotmail.com>
2016-01-21 16:49:11 +1100

    round new specs

--- app/controllers/admin/rounds_controller.rb
+++ app/controllers/admin/rounds_controller.rb
@@ -1,6 +1,16 @@
 class Admin::RoundsController < AdminsController
+  
   def index
       @competition = Competition.find(params[:competition_id])
       @rounds = @competition.rounds
+  end
+
+  def show
+    @competition = Competition.find(params[:competition_id])
+    @round = Round.find(params[:id])
+  end
+
+  def new
+    @competition = Competition.find(params[:competition_id])
   end
 end
