8d6d10ba413ea00efb156abb3bde5b56a9e32f83
figochen <figocia@hotmail.com>
2016-01-21 22:21:46 +1100

    new game and shared dialog

---
+++ app/controllers/admin/games_controller.rb
@@ -0,0 +1,5 @@
+class Admin::GamesController < AdminsController
+  def new
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -41,5 +41,6 @@ Rails.application.routes.draw do
     end
 
     resources :team_members, only: [:create, :destroy]
+    resources :games, only: [:create, :new]
   end
 end
