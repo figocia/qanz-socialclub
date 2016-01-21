d2cf14b638a17ea0585f78afaa3f324cb4fe653e
figochen <figocia@hotmail.com>
2016-01-22 01:55:46 +1100

    next match up methods

--- app/models/round.rb
+++ app/models/round.rb
@@ -11,4 +11,18 @@ class Round < ActiveRecord::Base
   def next
     competition.rounds.where("created_at > ?", created_at).first
   end
+
+  def next_match_up
+    teamAs = games.collect{|game| game.team_one_id}
+    teamBs = games.collect{|game| game.team_two_id}
+
+    teamA_last = teamAs.pop
+    teamB_last = teamBs.pop
+
+    # mismatch by one index between left and right
+    teamAs.push(teamB_last)
+    teamBs.unshift(teamA_last)
+
+    return {left: teamAs, right: teamBs }
+  end
 end
