b84b5f59413982e21d176afe3b0c9a863f8fb16f
figochen <figocia@hotmail.com>
2016-01-22 01:30:10 +1100

    round previous and next method

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -6,4 +6,20 @@ describe Round do
   it{ should validate_uniqueness_of(:name).scoped_to(:competition_id) }
 
   
+  describe '#previous' do
+    it 'returns the correct previous'  do
+      competition = Fabricate(:competition)
+      round1 = Fabricate(:round, name: 'round1', competition: competition )   
+      round2 = Fabricate(:round, name: 'round2', competition: competition )   
+      round3 = Fabricate(:round, name: 'round3', competition: competition )   
+
+      expect(round2.previous).to eq(round1)
+    end
+
+    it 'return nil for the first one' do
+      competition = Fabricate(:competition)
+      round1 = Fabricate(:round, competition: competition )   
+      expect(round1.previous).to be nil
+    end
+  end
 end
