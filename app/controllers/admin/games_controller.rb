638c085c6bc2c47acd37a4a60b20669e22ed79fd
figochen <figocia@hotmail.com>
2016-01-21 23:44:01 +1100

    new signle game actin

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -1,5 +1,16 @@
 class Admin::GamesController < AdminsController
+  before_action :find_round, only: [:new, :create]
+  
   def new
+    @game = Game.new
+  end
+
+  def create
     
+  end
+
+  private
+  def find_round
+    @round = Round.find(params[:round_id])
   end
 end
