c512156d1a6e5535564fee9d32aa1eed036f6ec2
figochen <figocia@hotmail.com>
2016-01-22 00:32:06 +1100

    create single game action complete

--- spec/controllers/admin/games_controller_spec.rb
+++ spec/controllers/admin/games_controller_spec.rb
@@ -17,9 +17,39 @@ describe Admin::GamesController do
   end
 
   describe 'POST Create' do
+    let(:teamA) {Fabricate(:team, competition: competition)}
+    let(:teamB) {Fabricate(:team, competition: competition)}
+
     it_behaves_like 'require_admin' do
       let(:action) { xhr :post, :create}
     end
+
+    context 'valid create' do
+      it 'creates the game successfully with right team' do
+        xhr :post, :create, game: {team_one_id: teamA.id, team_two_id: teamB.id, competition_id: competition.id, round_id: round.id}
+        expect(Game.first.team_two).to eq(teamB)
+      end
+
+      it 'creates the game with right association to competition' do
+        xhr :post, :create, game: {team_one_id: teamA.id, team_two_id: teamB.id, competition_id:  competition.id, round_id: round.id}
+        expect(Game.first.competition).to eq(competition)
+      end
+
+      it 'creates the game with right association to round' do
+        xhr :post, :create, game: {team_one_id: teamA.id, team_two_id: teamB.id, competition_id:  competition.id, round_id: round.id}
+        expect(Game.first.round).to eq(round)
+      end
+    end
+
+    context 'invalid create with same team selected' do
+      it 'does not create the game with same teams selected for both side' do
+        xhr :post, :create, game: {team_one_id: teamB.id, team_two_id: teamB.id, competition_id:  competition.id, round_id: round.id}
+        expect(Game.all.size).to eq(0)
+      end
+    end
+
+    
+
   end
 
 end
