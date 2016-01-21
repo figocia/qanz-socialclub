374707863007caa11b9e25fa13714b5ff92f1b17
figochen <figocia@hotmail.com>
2016-01-22 01:30:10 +1100

    round previous and next method

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,4 +3,12 @@ class Round < ActiveRecord::Base
   has_many :games, :dependent => :destroy
 
   validates_uniqueness_of :name, scope: :competition_id
+
+  def previous
+    competition.rounds.where("created_at < ?", created_at).last
+  end
+
+  def next
+    competition.rounds.where("created_at > ?", created_at).first
+  end
 end
