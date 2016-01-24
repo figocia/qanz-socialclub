3d84455a1eb844f4ae50ee8a37d8510de6a27963
figochen <figocia@hotmail.com>
2016-01-25 03:19:17 +1100

    carrierwave uploader

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -4,6 +4,14 @@ class Admin::EventsController < AdminsController
   end
 
   def new
-    @event = Event.new    
+    @event = Event.new
+    respond_to do |format|
+      format.js
+    end
+  end
+  
+  private
+  def event_params
+    params.require(:event).permit(:name, :time, :address, :image, :member_only, :non_member_fee, :description)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120145948) do
+ActiveRecord::Schema.define(version: 20160124154619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160120145948) do
     t.float    "longitude"
     t.boolean  "member_only",                            default: false
     t.decimal  "non_member_fee", precision: 5, scale: 2
+    t.string   "image"
   end
 
   create_table "games", force: true do |t|
