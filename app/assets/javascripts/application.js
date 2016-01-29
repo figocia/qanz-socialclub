2f7430a49a21e0daf46e677e89604af7d916b496
figochen <figocia@hotmail.com>
2016-01-30 01:51:09 +1100

    asset clobber

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,7 @@
 //= require underscore
 //= require gmaps/google
 //= require turbolinks
+
 //= require_tree .
 
 
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module QanzSocialclub
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Melbourne'
-    # config.assets.enabled = true    
+    config.assets.enabled = true    
+    config.serve_static_assets = true
 
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
